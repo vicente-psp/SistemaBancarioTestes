32e910bd945b413f8d41564a992911ba34c28d40
Vicente Silva <vicente.pspenha@gmail.com>
2018-06-21 23:46:23 -0300

    EContaCorrenteTest

--- test/entidade/EContaCorrenteTest.java
+++ test/entidade/EContaCorrenteTest.java
@@ -1,56 +1,151 @@
 package entidade;
 
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
-import junit.framework.TestCase;
-
 /**
+ * @author Tony
  *
- * @author Vicente
  */
-public class EContaCorrenteTest extends TestCase {
-	
-	private EContaCorrente cc;
-	private float saldo = 158.58f;
-	private String agencia = "752";
-	private String conta = "2568";
-	private String cliente = "2568";
-    
-    @Before
-    public void setUp()throws Exception{
-        cc = new EContaCorrente();
-        cc = new EContaCorrente();
-        cc.setAgencia(agencia);
-        cc.setConta(conta);
-        cc.setCliente(cliente);
-        cc.setSaldo(saldo);
-    }
-    
-//    @Test(expected = IllegalArgumentException.class)
-//    public void testGetSaldo() throws Exception{
-//    	String agencia = "752";
-//    	String conta = "2568";
-//    	String cliente = "2568";
-//    	float saldo = (float) 158.58;
-//        cc = new EContaCorrente();
-//        cc.setAgencia(agencia);
-//        cc.setConta(conta);
-//        cc.setCliente(cliente);
-//        cc.setSaldo(saldo);
-//    	
-//    	float resultaEsperado = (float) 158.58;
-//    	assertEquals(resultaEsperado, cc.getSaldo(), 0.00001);
-//    	fail("Era esperado um erro que nao aconteceu");
-//    }
-    
-    @Test
-    public void testGetSaldo(){
-        System.out.println("Testando caso de testes 01");
-        EContaCorrente instance = new EContaCorrente();
-        float expResult = 0.0f;
-        float result = 0.0f;
-        assertEquals(expResult, result, 0.0f);
-    }
-    
+public class EContaCorrenteTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link entidade.EContaCorrente#getAgencia()}.
+	 */
+	@Test
+	public void testGetAgencia() {
+		try {
+			EContaCorrente cc = new EContaCorrente();
+			cc.getAgencia();
+		} catch (Exception e) {
+			fail("Teste falhou");
+		}
+	}
+
+	/**
+	 * Test method for {@link entidade.EContaCorrente#setAgencia(java.lang.String)}.
+	 */
+	@Test
+	public void testSetAgencia() {
+		try {
+			EContaCorrente cc = new EContaCorrente();
+			cc.setAgencia("123456-X");
+		} catch (Exception e) {
+			fail("Teste falhou");
+		}
+	}
+
+	/**
+	 * Test method for {@link entidade.EContaCorrente#getConta()}.
+	 */
+	@Test
+	public void testGetConta() {
+		try {
+			EContaCorrente cc = new EContaCorrente();
+			cc.getConta();
+		} catch (Exception e) {
+			fail("Teste falhou");
+		}
+	}
+
+	/**
+	 * Test method for {@link entidade.EContaCorrente#setConta(java.lang.String)}.
+	 */
+	@Test
+	public void testSetConta() {
+		try {
+			EContaCorrente cc = new EContaCorrente();
+			cc.setConta("123456");
+		} catch (Exception e) {
+			fail("Teste falhou");
+		}
+	}
+
+	/**
+	 * Test method for {@link entidade.EContaCorrente#getCliente()}.
+	 */
+	@Test
+	public void testGetCliente() {
+		try {
+			EContaCorrente cc = new EContaCorrente();
+			cc.getCliente();
+		} catch (Exception e) {
+			fail("Teste falhou");
+		}
+	}
+
+	/**
+	 * Test method for {@link entidade.EContaCorrente#setCliente(java.lang.String)}.
+	 */
+	@Test
+	public void testSetCliente() {
+		try {
+			EContaCorrente cc = new EContaCorrente();
+			cc.setCliente("João");
+		} catch (Exception e) {
+			fail("Teste falhou");
+		}
+	}
+
+	/**
+	 * Test method for {@link entidade.EContaCorrente#getSaldo()}.
+	 */
+	@Test
+	public void testGetSaldo() {
+		try {
+			EContaCorrente cc = new EContaCorrente();
+			cc.getSaldo();
+		} catch (Exception e) {
+			fail("Teste falhou");
+		}
+	}
+
+	/**
+	 * Test method for {@link entidade.EContaCorrente#setSaldo(float)}.
+	 */
+	@Test
+	public void testSetSaldo() {
+		try {
+			EContaCorrente cc = new EContaCorrente();
+			cc.setSaldo(0.0f);
+		} catch (Exception e) {
+			fail("Teste falhou");
+		}
+		
+		
+	}
+
 }
